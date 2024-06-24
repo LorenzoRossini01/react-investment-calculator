754f2b28a27e93a7640ded60a988ff70d9fe5f64
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-24 13:21:14 +0200

    feat: create userinput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
   return (
     <>
       <Header>React Investment Calculator</Header>
+      <UserInput />
     </>
   );
 }
