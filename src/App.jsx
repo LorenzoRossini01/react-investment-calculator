a626bfd2419d12027e21b30bddd341f3e697ebb3
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-24 13:33:59 +0200

    feat: add useState object to gather input field data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput({
+      ...userInput,
+      [inputIdentifier]: newValue,
+    });
+  }
   return (
     <>
       <Header>React Investment Calculator</Header>
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
     </>
   );
 }
